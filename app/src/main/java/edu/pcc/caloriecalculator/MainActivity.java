ba73594f469cc8701fec0cd1202ec181d5955363
Brian Tripp <btripp365@gmail.com>
2026-05-18 10:40:13 -0500

    refactor: migrate MainActivity to ViewBinding

--- app/src/main/java/edu/pcc/caloriecalculator/MainActivity.java
+++ app/src/main/java/edu/pcc/caloriecalculator/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.pcc.caloriecalculator;
 
+import edu.pcc.caloriecalculator.databinding.ActivityMainBinding;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,18 +8,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     public final static String EXTRA_WEIGHT = "com.murach.caloriesburnedcalculator.WEIGHT";
     public final static String EXTRA_MET = "com.murach.caloriesburnedcalculator.MET";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 
     @Override
