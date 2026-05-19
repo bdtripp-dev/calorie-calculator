dd9e18018ce23e16964b1bfe0f391f424009ca69
Brian Tripp <btripp365@gmail.com>
2026-05-19 11:14:16 -0500

    refactor: replace default dialog title bar with a custom centered TextView

--- app/src/main/java/edu/pcc/caloriecalculator/AboutActivity.java
+++ app/src/main/java/edu/pcc/caloriecalculator/AboutActivity.java
@@ -2,6 +2,7 @@ package edu.pcc.caloriecalculator;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
@@ -9,6 +10,9 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_about);
     }
 
