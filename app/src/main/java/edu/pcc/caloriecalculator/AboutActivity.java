28b7f108bfefbcfe836b135fad81956101d8d06e
Brian Tripp <btripp365@gmail.com>
2026-05-20 11:27:39 -0500

    refactor(ui): replace XML onClick with Java click listener for About screen

--- app/src/main/java/edu/pcc/caloriecalculator/AboutActivity.java
+++ app/src/main/java/edu/pcc/caloriecalculator/AboutActivity.java
@@ -3,6 +3,7 @@ package edu.pcc.caloriecalculator;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
@@ -14,6 +15,15 @@ public class AboutActivity extends AppCompatActivity {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_about);
+
+        Button buttonClose = findViewById(R.id.buttonClose);
+
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     public void dismiss(View view) {
